3b45735596f976eed9751a45e300be1db50b2810
Jaredloc <lockhartjared@gmail.com>
2020-02-23 12:46:36 -0500

    #12
    A prototype for main menu screen

--- app/src/main/java/com/example/ub_eats/MainScreen.java
+++ app/src/main/java/com/example/ub_eats/MainScreen.java
@@ -1,5 +1,6 @@
 package com.example.ub_eats;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,22 +9,15 @@ import android.widget.Button;
 import android.os.Bundle;
 import android.widget.ViewSwitcher;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
     }
-
 
     public void onClick(View view) {
         Intent intent = new Intent(this, Registration.class);
-        startActivity(intent);
-    }
-
-    public void onClickMain(View view){
-        Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
     }
 }
