46d6bb9c9704c103280fd71ee5439a03234e7601
Ayesha Ismail <aismail2@buffalo.edu>
2020-02-23 12:15:44 -0500

    update
    #16 #12 #13 #

---
+++ app/src/main/java/com/example/myapplication/OrderConfirmation.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class OrderConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirmation);
+
+        final Random rdnum=new Random();
+        final TextView textGen=findViewById(R.id.textView14);
+        textGen.setText(String.valueOf(rdnum.nextInt(200)));
+
+    }
+    public void onClickk(View view) {
+        openActivity();
+
+    }
+
+    private void openActivity() {
+
+        Intent intent=new Intent(this, MenuMain.class);
+        startActivity(intent);
+
+
+    }
+}
