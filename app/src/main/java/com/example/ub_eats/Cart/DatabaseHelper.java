4899cbc39347517179cb8bd60026e3cda4dcbbf2
Ayesha Ismail <aismail2@buffalo.edu>
2020-04-12 08:58:19 -0400

    Added multiple functionality to cart such as: update quantity, delete item #53 #52 Added search view to menu items #61

--- app/src/main/java/com/example/ub_eats/Cart/DatabaseHelper.java
+++ app/src/main/java/com/example/ub_eats/Cart/DatabaseHelper.java
@@ -12,10 +12,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME="mylist.db";
     public static final String TABLE_NAME="mylist_data";
-    public static final String COL3="Resturant name";
+  //  public static final String COL3="Resturant name";
     public static final String COL1="item";
     public static final String COL2="price";
-
+    public static final String COL3="quantity";
 
 
     public DatabaseHelper(Context context){
@@ -23,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + TABLE_NAME+ " (item TEXT, price TEXT)");
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME+ " (item TEXT, price TEXT, quantity TEXT)");
     }
 
     @Override
@@ -34,19 +34,43 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public boolean addData(String item, String price){
         SQLiteDatabase db= this.getWritableDatabase();
+        int quantity=1;
         ContentValues contentValues= new ContentValues();
         contentValues.put(COL1, item);
         contentValues.put(COL2, price);
-        // db.insert(TABLE_NAME, null, contentValues);
+        contentValues.put(COL3, quantity);
 
-        long result=db.insert(TABLE_NAME,null,contentValues);
-        if(result==-1){
-            return false;
+        if(Exists(item)){
+            updateQuantity(item); //Item Already exists, update quantity
+
         }else{
-            return true;
+            long result=db.insert(TABLE_NAME,null,contentValues);
+           // cursor.close();
+            if(result==-1){
+                return false;
+            }else{
+                return true;
 
+            }
         }
+       // cursor.close();
+        return true;
 
+    }
+
+    public boolean Exists(String item) {
+
+        String[] columns = { COL1};
+        String selection = COL1 + " =?";
+        String[] selectionArgs = { item };
+        String limit = "1";
+
+        SQLiteDatabase db= this.getWritableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, columns, selection, selectionArgs, null, null, null, limit);
+        boolean exists = (cursor.getCount() > 0);
+        cursor.close();
+        return exists;
     }
     public Cursor getListContents(){
         SQLiteDatabase db=this.getWritableDatabase();
@@ -57,4 +81,75 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db=this.getWritableDatabase();
         db.delete(TABLE_NAME, null, null);
     }
+    public boolean updateQuantity(String item){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues val=new ContentValues();
+
+        String getQuantity= quanRet(item);
+        int update= Integer.parseInt(getQuantity);
+        update++;
+        val.put(COL3, update);
+        //String newQuan="2";
+        db.update(TABLE_NAME, val, COL1 + "= ?", new String[] {item});
+        return true;
+    }
+    public boolean incrementQuantity(String item){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues val=new ContentValues();
+
+        String getQuantity= quanRet(item);
+        int update= Integer.parseInt(getQuantity);
+        update++;
+        val.put(COL3, update);
+        //String newQuan="2";
+        db.update(TABLE_NAME, val, COL1 + "= ?", new String[] {item});
+        //quanRet(item);
+        return true;
+    }
+    public boolean decrementQuantity(String item){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues val=new ContentValues();
+
+        String getQuantity= quanRet(item);
+        int update= Integer.parseInt(getQuantity);
+        if(update>1){
+            update--;
+            val.put(COL3, update);
+            db.update(TABLE_NAME, val, COL1 + "= ?", new String[] {item});
+            //quanRet(item);
+            return true;
+        }else{
+            return true;
+        }
+
+    }
+    public String quanRet (String itemName){
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        String s1,s2,s3;
+        s3="1";
+
+        Cursor cursor= db.rawQuery("select * from "+ TABLE_NAME+ " WHERE "+ COL1 +" =? ",new String[]{itemName});
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                s3 = cursor.getString(cursor.getColumnIndex(COL3));
+
+
+            }while (cursor.moveToNext());
+        }
+        return s3;
+    }
+    public void deleteItem(String itemName){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.delete(TABLE_NAME, COL1 + " = ? ", new String[]{itemName});
+        //db.close();
+
+    }
+    /*public void deleteItem(String col, String item){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.delete(TABLE_NAME, COL1 + "=" + item, null) ;
+        db.close();
+    }*/
 }
--- app/src/main/java/com/example/ub_eats/Menu/myAdapterr.java
+++ app/src/main/java/com/example/ub_eats/Menu/myAdapterr.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,22 +18,45 @@ import com.example.ub_eats.Cart.DatabaseHelper;
 import com.example.ub_eats.Cart.cart;
 import com.example.ub_eats.R;
 
-public class myAdapterr extends RecyclerView.Adapter<myAdapterr.MyViewHolderr> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class myAdapterr extends RecyclerView.Adapter<myAdapterr.MyViewHolderr> implements Filterable {
     public static final String EXTRA_MESSAGE2="com.example.ub_eats.Menu.MESSAGE";
     public static final String EXTRA_MESSAGE3="com.example.ub_eats.Menu.MESSAGE";
     DatabaseHelper myDB;
 
 
     String data1[], data2[],data3[];
-    int images[];
-    Context context;
 
-    public myAdapterr(Context ct, String s1[], String s2[],  String s3[],int img[]){
+    Context context;
+    private List<String> exampleListFullName;
+    private List<String> exampleListFullprice;
+    private List<String> exampleListFulls3;
+    List<String> listFilteredPrice;
+    List<String> itemList;
+    List<String> listFilteredName;
+    List<String> listFiltereds3;
+
+
+
+    public myAdapterr(Context ct, String s1[], String s2[],  String s3[]){
         context=ct;
         data1=s1;
         data2=s2;
         data3=s3;
-        images=img;
+        exampleListFullName = new ArrayList<>(Arrays.asList(data1));
+        exampleListFullprice = new ArrayList<>(Arrays.asList(data2));
+        exampleListFulls3 = new ArrayList<>(Arrays.asList(data3));
+        itemList  = new ArrayList<String>( Arrays.asList(data1));
+        listFiltereds3= new ArrayList<>();
+
+    }
+    @Override
+    public Filter getFilter() {
+        return filter;
     }
 
     @NonNull
@@ -49,7 +74,6 @@ public class myAdapterr extends RecyclerView.Adapter<myAdapterr.MyViewHolderr> {
         holder.mytext1.setText(data1[position]);
         holder.mytext2.setText(data2[position]);
         holder.mytext3.setText(data3[position]);
-        holder.myImage.setImageResource(images[position]);
 
         holder.bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,14 +94,76 @@ public class myAdapterr extends RecyclerView.Adapter<myAdapterr.MyViewHolderr> {
 
     @Override
     public int getItemCount() {
-        return images.length;
+        return data2.length;
     }
+
+
+    Filter filter = new Filter() {
+        @Override //background thread
+        protected FilterResults performFiltering(CharSequence charSequence) {
+
+                        listFilteredName= new ArrayList<>();
+                         listFilteredPrice= new ArrayList<>();
+                         listFiltereds3=new ArrayList<>();
+
+            //List<String> itemName = new ArrayList<String>(Arrays.asList(data1));
+
+            if( charSequence.toString().isEmpty()){
+
+                listFilteredName.addAll(exampleListFullName);
+                listFilteredPrice.addAll(exampleListFullprice);
+                listFiltereds3.addAll(exampleListFulls3);
+
+
+            }else{
+                System.out.println("-------------------");
+                System.out.println(data2.length);
+                for (int i=0; i<data2.length;i++){
+
+                    if (exampleListFullName.get(i).toLowerCase().contains(charSequence.toString().toLowerCase())){
+                        listFilteredName.add(exampleListFullName.get(i));
+                        listFilteredPrice.add(exampleListFullprice.get(i));
+                        listFiltereds3.add(exampleListFulls3.get(i));
+                    }
+                }
+            }
+          //  data2=exampleListFullprice.toArray(new String[0]);
+            FilterResults filterResults=new FilterResults();
+            filterResults.values=listFilteredName;
+
+            System.out.println("---List FIltered size----------------");
+            System.out.println(listFilteredName.size());
+            return filterResults;
+        }
+
+        @Override //UI thread
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            List<String> priceList = new ArrayList<String>(Arrays.asList(data2));
+            List<String> s3List = new ArrayList<String>(Arrays.asList(data3));
+
+
+            //itemList.clear();
+            priceList.clear();
+            s3List.clear();
+
+           // exampleListFull.clear(); //
+            itemList.clear();
+            itemList.addAll((Collection<? extends String>) filterResults.values);
+            priceList.addAll(listFilteredPrice);
+            s3List.addAll(listFiltereds3);
+            data1=itemList.toArray(new String[0]);
+            data2=priceList.toArray(new String[0]);
+            data3=s3List.toArray(new String[0]);
+
+            notifyDataSetChanged();
+
+        }
+    };
 
     public class MyViewHolderr extends RecyclerView.ViewHolder {
 
 
         TextView mytext1, mytext2, mytext3;
-        ImageView myImage;
         Button bt;
 
         public MyViewHolderr(@NonNull View itemView) {
@@ -86,10 +172,8 @@ public class myAdapterr extends RecyclerView.Adapter<myAdapterr.MyViewHolderr> {
             mytext1=itemView.findViewById(R.id.myText1);
             mytext2=itemView.findViewById(R.id.myText2);
             mytext3=itemView.findViewById(R.id.myText3);
-            myImage=itemView.findViewById(R.id.myImageView);
             bt=itemView.findViewById(R.id.button);
 
-            myImage.setVisibility(View.GONE);
         }
     }
 
